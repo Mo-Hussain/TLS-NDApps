ee4b343e004a98da49dc9950875b4aaff7af3ffd
Matthew Morrison <ewmo11@gmail.com>
2018-02-04 21:56:12 +0000

    Added ability to input scores

--- BettingDapp3/contracts/Betting.sol
+++ BettingDapp3/contracts/Betting.sol
@@ -1,5 +1,13 @@
 pragma solidity ^0.4.15;
 
+/*
+Notes:
+- Need to have a function that checks TLS proof of input
+
+
+
+*/
+
 contract Betting {
 
   /* Structure to hold a specific football fixture */
@@ -7,6 +15,8 @@ contract Betting {
     uint fixtureID;
     string home;
     string away;
+    uint home_score;
+    uint away_score;
   }
 
   /* Structure to hold a bet between two parties */
@@ -19,6 +29,7 @@ contract Betting {
     uint amount;
     uint proposer_home_score;
     uint proposer_away_score;
+    address winner;
     mapping (uint => Fixture) fixture;
   }
 
@@ -43,19 +54,18 @@ contract Betting {
     master = msg.sender;
     betCount = 0;
     fixtureCount = 0;
-    /* addFixture("Arsenal", "Chelsea"); */
   }
 
   /* Function to add a fixture to the contract */
    function addFixture(string _home, string _away) public onlyMaster returns (uint fixtureID){
     fixtureID = fixtureCount++;
-    fixtures[fixtureID] = Fixture(fixtureID,_home,_away);
+    fixtures[fixtureID] = Fixture(fixtureID,_home,_away,0,0);
   }
 
   /* Function allowing a user to propose a bet */
   function proposeBet(uint _fixtureID, uint _odds, uint _amount, uint _home_score, uint _away_score) public returns (uint betID) {
     betID = betCount++;
-    bets[betID] =  Bet(betID, msg.sender, 0, false, _odds, _amount,_home_score,_away_score);
+    bets[betID] =  Bet(betID, msg.sender, 0, false, _odds, _amount,_home_score,_away_score,0);
     bets[betID].fixture[0] = fixtures[_fixtureID];
   }
 
@@ -69,11 +79,36 @@ contract Betting {
     return fixtures[_fixtureID].away;
   }
 
+  /* Need to be able to get number of fixtures and number of bets */
+  function getNumberOfBets() public constant returns (uint) {
+    return betCount;
+  }
+
+  function getNumberOfFixtures() public constant returns (uint) {
+    return fixtureCount;
+  }
+
+  /* Function to accept bet */
+  function acceptBet(uint _betID) public {
+    betCount--;
+    bets[_betID].acceptor = msg.sender;
+    bets[_betID].accepted = true;
+  }
+
+  /* Function to input the scores of a fixture */
+  function inputScore(uint _fixtureID, uint _home_score, uint _away_score) public {
+    fixtures[_fixtureID].home_score = _home_score;
+    fixtures[_fixtureID].away_score = _away_score;
+  }
+
+  /* Function to get the winner of a bet */
+
 
   /* Function to get all outstanding bets that have not been accepted */
+  /* Matt - I think we should do this on the front-end, looping through bets and just getting those accepted */
 
 
-  /* // add all teams
+  /* // add all teams - Matt - I am not sure we need this
   function addTeams(){
 
   }
